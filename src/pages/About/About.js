6180b2690741863383ab84dac43192ae6cf4b8c3
BapiMajumder1402 <nbbapim@gmail.com>
2023-04-20 18:52:45 +0530

    Responsive About page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -4,10 +4,9 @@ import a from "./About.module.css";
 
 export default function About() {
   return (
-    <>
     <div className={a.container}>
-      <div className={a.left}>
-        <div className={a.Lvideo}>
+      <div className={a.up}>
+        <div className={a.content}>
           <iframe
             src="https://www.youtube.com/embed/liBq75smdo8"
             title="YouTube video player"
@@ -16,40 +15,27 @@ export default function About() {
             allowfullscreen
           ></iframe>
         </div>
-        <div className={a.para}>
-          <p>
-            Being physically and mentally fit is necessary to live a happy, long
-            life. Exercise is one of the best ways to keep a person healthy.
-            Hence, it is always best to find a workout routine no matter how
-            busy you are. With the numerous diseases that spread today, many
-            individuals realized the essence of workout. Specifically, having a
-            workout routine will give an individual the greatest physical,
-            mental, and social benefits.
-          </p>
+        <div className={a.content}>
+          
+         <p>Lorem ipsum dolor sit amet consectetur <span>Lorem ipsum dolor sit amet.</span> adipisicing elit. Error modi qui magnam quidem? Temporibus quo laborum cum <span>Lorem ipsum dolor sit amet.</span> veniam deserunt veritatis earum quia rerum ipsa, optio pariatur placeat voluptatum, vero sit?</p> 
         </div>
+
       </div>
-      <div className={a.right}>
-        <div>
-          <p className={a.para}>
-            Accordingly, exercise will help you increase energy levels, reduce
-            chronic disease risk, lose weight, and help improve brain health and
-            memory. With such benefits, you probably will love to do workout
-            routines soon. Luckily, you don’t need to do it yourself as various
-            personal trainers, or professional fitness coaches provide the help
-            you need. And joining fitness classes is just at your fingertips.
-          </p>
+      <div className={a.down}>
+        <div className={a.content}>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.<span>Lorem ipsum dolor sit amet.</span> Odit, dignissimos! Iure porro modi eum dolorem ipsa voluptates <span>Lorem ipsum dolor sit amet.</span> molestiae ullam ab fuga illum hic est voluptatem ipsam, expedita neque accusantium non!</p></div>
+        <div className={a.content}><iframe
+          src="https://www.youtube.com/embed/XP1Ne5mBn_A"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
         </div>
-        <div className={a.Rvideo}>
-          <iframe
-            src="https://www.youtube.com/embed/XP1Ne5mBn_A"
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen
-          ></iframe>
-        </div>
+
       </div>
+
+
     </div>
-    </>
-  );
+  )
 }
